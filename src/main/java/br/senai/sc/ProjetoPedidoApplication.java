ea29a7b0413949243baaad62fbb3ed25fe10811b
Viviany Pfleger <pflegerviviany@gmail.com>
2019-03-12 19:28:47 -0300

    Categoria

--- src/main/java/br/senai/sc/ProjetoPedidoApplication.java
+++ src/main/java/br/senai/sc/ProjetoPedidoApplication.java
@@ -1,13 +1,37 @@
 package br.senai.sc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.senai.sc.domain.Categoria;
+import br.senai.sc.domain.Produto;
+import br.senai.sc.repositories.CategoriaRepository;
+import br.senai.sc.repositories.ProdutoRepository;
+
 @SpringBootApplication
-public class ProjetoPedidoApplication {
+public class ProjetoPedidoApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoPedidoApplication.class, args);
+	}
+
+	@Autowired
+	private CategoriaRepository repo1;
+	private ProdutoRepository repo2;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Produto pro1 = new Produto(null, "Mouse", 19.99);
+		Produto pro2 = new Produto(null, "Teclado", 14.99);
+		
+		repo1.save(cat1);
+		repo1.save(cat2);
+		repo2.save(pro1);
+		repo2.save(pro2);
 	}
 
 }
--- src/main/java/br/senai/sc/domain/Produto.java
+++ src/main/java/br/senai/sc/domain/Produto.java
@@ -6,36 +6,42 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Categoria {
+public class Produto {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double preco;
 	
-	public Categoria() {
+	public Produto() {
 		
 	}
 	
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preco) {
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
-
+	
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
 	
 }
